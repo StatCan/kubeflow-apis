d78874a3e90c78f4b183c3fac56cb4b68ca795fe
Zachary Seguin <zachary.seguin@statcan.gc.ca>
2022-02-17 11:56:27 -0500

    feat: Add kubeflow APIs + generated clientset/listers/informers

---
+++ apis/kubeflow/register.go
@@ -0,0 +1,6 @@
+package kubeflow
+
+// GroupName is the group name used in this package
+const (
+	GroupName = "kubeflow.org"
+)
---
+++ apis/kubeflow/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// +k8s:deepcopy-gen=package
+// +groupName=kubeflow.org
+
+// Package v1alpha is the v1alpha1 version of the API.
+package v1alpha1
